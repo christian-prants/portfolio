eca4083f449dc070684533c8259578bb769d3708
Christian <chris.prants9@gmail.com>
2026-07-16 17:55:20 -0300

    installed missing dependencies for gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,13 @@ import IconLinkedIn from './assets/icon/linkedin.svg?react';
 import Home from "./pages/home";
 import About from "./pages/about";
 import Project from "./pages/project";
-
 import Footer from "./components/footer";
-
 import Navbar from "./components/navbar";
-
 import './styles/main.scss';
-
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
